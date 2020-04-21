55f7ece780a530be8983ef12d0dc48123d6ae9c3
Brett Dutro <brett.dutro@gmail.com>
2020-04-21 16:44:20 -0500

    Add gmmlib from master

---
+++ packages/multimedia/gmmlib/package.mk
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="gmmlib"
+PKG_VERSION="19.4.1"
+PKG_SHA256="bb874b41c499abb8f6253b1834e93a02ed9744de71f2503ee9cd4100af7c1860"
+PKG_ARCH="x86_64"
+PKG_LICENSE="MIT"
+PKG_SITE="https://01.org/linuxmedia"
+PKG_URL="https://github.com/intel/gmmlib/archive/intel-gmmlib-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="gmmlib: The Intel(R) Graphics Memory Management Library provides device specific and buffer management for the Intel(R) Graphics Compute Runtime for OpenCL(TM) and the Intel(R) Media Driver for VAAPI."
+
+PKG_CMAKE_OPTS_TARGET="-DRUN_TEST_SUITE=OFF"
