4f7c4647e7c634a5df4d3e1d902165a61777f1aa
Brett Dutro <brett.dutro@gmail.com>
2020-04-22 11:04:21 -0500

    Grab glm, libdrm, mesa, llbm, dosfstools, and x11 packages from master

--- packages/x11/proto/xorgproto/package.mk
+++ packages/x11/proto/xorgproto/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xorgproto"
-PKG_VERSION="2018.4"
-PKG_SHA256="fee885e0512899ea5280c593fdb2735beb1693ad170c22ebcc844470eec415a0"
+PKG_VERSION="2019.2"
+PKG_SHA256="46ecd0156c561d41e8aa87ce79340910cdf38373b759e737fcbba5df508e7b8e"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="https://xorg.freedesktop.org/archive/individual/proto/$PKG_NAME-$PKG_VERSION.tar.bz2"
